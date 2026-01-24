606e000873fcb03d979d03717e4d919e85b4c7a8
lj <2069057986@qq.com>
2026-01-24 10:49:47 +0800

    fix: 重构项目-不直接实现复杂完整系统，转而先跑通基本流程！

---
+++ src/main/java/com/lj/DeepLink.java
@@ -0,0 +1,18 @@
+package com.lj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName DeepLink
+ * @Description
+ * @Author Dark Chocolate 2069057986@qq.com
+ * @Date 2026/1/24 10:44
+ * @Version JDK 17
+ */
+@SpringBootApplication
+public class DeepLink {
+    public static void main(String[] args) {
+        SpringApplication.run(DeepLink.class, args);
+    }
+}
